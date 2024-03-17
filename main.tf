786912b0c6b3215048de28f0ba2910d233490e41
palgitaccount <v.pallav@gmail.com>
2024-03-17 12:53:57 +0530

    Update main.tf to create file

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+resource "local_file" "foo" {
+  content  = "this terraform scripts is run by jenkins"
+  filename = "/home/bob/foo.bar"
+}
+
 output "jenkins_terraform" {
   value = "running Terraform from Jenkins"
 }
